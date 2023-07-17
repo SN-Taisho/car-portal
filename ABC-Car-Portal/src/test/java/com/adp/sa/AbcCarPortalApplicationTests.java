d7e34d1781bbc2d4bc7f107aa282d4175c332f2b
Anzel <112552105+SN-Taisho@users.noreply.github.com>
2023-07-17 10:10:16 +0800

    Uploaded the project files

---
+++ ABC-Car-Portal/src/test/java/com/adp/sa/AbcCarPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adp.sa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcCarPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
